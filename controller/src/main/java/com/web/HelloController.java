3326f6d57873dfc6ee2682922130352977831f93
吕栋伟-亚信 <lvdw@asiainfo.com>
2019-01-14 20:41:27 +0800

    添加页面

--- controller/src/main/java/com/web/HelloController.java
+++ controller/src/main/java/com/web/HelloController.java
@@ -4,34 +4,34 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.iservice.ITestSV;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.Null;
 
 /**
  * Created by lvdw on 2018/8/24 0024.
  */
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-@RestController
+
+@Controller
 public class HelloController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-     /*@Reference(url = "dubbo://localhost:20880")*/
- @Reference()
-private ITestSV iTestSV;
+    /*@Reference(url = "dubbo://localhost:20880")*/
+    @Reference()
+    private ITestSV iTestSV;
+
     @RequestMapping("/hello")
     public String index() {
-        int id=1;
-        logger.debug("hahah{}",id);
+        int id = 1;
+        logger.debug("hahah{}", id);
         logger.warn("警告");
         logger.info("标注");
         logger.debug("标注");
-        String var=iTestSV.sayHello();
-       return var+" World";
+        String var = iTestSV.sayHello();
+        return "/service.html";
 
     }
 
